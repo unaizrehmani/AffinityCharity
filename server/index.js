d2d293d21e71869ba503f9f9daa77c0c030b60ca
Unaiz Rehmani <unaizrehmani@gmail.com>
2019-03-10 12:43:20 -0400

    remove local variables

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json());
 
 // CONFIG
 console.log("process.env: ", process.env);
-const DB_NAME = process.env.DB_NAME || "test";
-const DB_USER = process.env.DB_USER || "unaizrehmani";
-const DB_PASSWORD = process.env.DB_PASSWORD || "iv5RL35QNJ3NHLRF";
-const PORT = process.env.PORT || 8000;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const PORT = process.env.PORT;
 
 // ROUTES
 app.get("/", (req, res, next) => res.send(process.env));
