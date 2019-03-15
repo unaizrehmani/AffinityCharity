70b1cdaf5dd448616db0fedd98cfeff366d72764
Unaiz Rehmani <unaizrehmani@gmail.com>
2019-03-15 02:45:23 -0400

    remove console.log statement

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,7 +2,6 @@ const User = require("../models/user");
 
 exports.authToken = async (req, res, next) => {
   const { email, password } = req.body;
-  console.log(email, password);
   try {
     const user = await User.authenticate(email, password);
     res.send(user.generateAuthToken());
