333e521067cf604d721d56812226d02540de378c
Rami Taleb <rtale089@uottawa.ca>
2019-03-16 15:03:27 -0400

    add routes for the agents

---
+++ server/routes/agents.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const multiPartMiddleware = require("connect-multiparty")();
+const agentController = require("../controllers/agents");
+const sanitizeBody = require("../middleware/sanitization/sanitizeBody");
+
+router.use(sanitizeBody);
+
+//POST routes
+router.post("/", multiPartMiddleware, agentsController.insertAgent);
+
+//GET routes
+router.get("/", agentController.getAllAgent);
+router.get("/:agentID", agentController.getAgentByID);
+
+//PATCH routes
+router.patch("/:agentID", agentController.patchAgentByID);
+
+//DELETE routes
+router.delete("/:agentID", agentController.deleteAgentByID);
+
+module.exports = router;
