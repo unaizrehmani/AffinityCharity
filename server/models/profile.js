bf01f61f1cd429fc403fce2b5f2609460f6549af
Pasoon Azimi <pasoon.azimi10@gmail.com>
2019-03-16 20:06:52 -0400

    model for profile

---
+++ server/models/profile.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  location:{
+    type: String
+  },
+  mediaURL: {
+    type: String,
+    required: false
+  },
+  imageID: {
+    type: String,
+    require: false
+  },
+  charityID: {
+    type: String,
+    require: true
+  },
+  posts: {
+    type: Array,
+    require: false
+  },
+  createdDate: {
+    type: Date,
+    required: true
+  }
+});
+
+postSchema.methods.toJSON = function() {
+  var obj = this.toObject();
+  delete obj.__v;
+  return obj;
+};
+
+module.exports = mongoose.model("Post", postSchema);
