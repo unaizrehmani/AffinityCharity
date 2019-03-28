0fd449ecd18aa69341c4bbbee33a227c2e624d42
Unaiz Rehmani <unaizrehmani@gmail.com>
2019-03-28 17:35:18 -0400

    clean up some comments

--- server/middleware/auth/verifyToken.js
+++ server/middleware/auth/verifyToken.js
@@ -2,11 +2,10 @@ const secret = process.env.JWT_TOKEN_SECRET
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    let token = req.headers["x-access-token"] || req.headers["authorization"]; // Express headers are auto converted to lowercase
+    let token = req.headers["x-access-token"] || req.headers["authorization"];
 
     if (token) {
         if (token.startsWith("Bearer ")) {
-            // Remove Bearer from string
             token = token.slice(7, token.length);
         }
         jwt.verify(token, secret, (err, decoded) => {
