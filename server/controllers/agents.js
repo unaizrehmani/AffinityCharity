29b530a5bf92822dcb5bc7feee98e56642577c0e
Unaiz Rehmani <unaizrehmani@gmail.com>
2019-03-28 18:53:50 -0400

    add update cloudinary capability to agent

--- server/controllers/agents.js
+++ server/controllers/agents.js
@@ -2,7 +2,7 @@ const Agent = require('../models/agent');
 const cloudinaryUtil = require('../middleware/cloudinary/cloudinary');
 const bcrypt = require('bcrypt');
 const SALTROUNDS = 14;
-const cloudinaryPath = process.env.CLOUDINARY_PATH;
+const cloudinaryPath = `${process.env.CLOUDINARY_PATH}/agents`;
 
 // POST routes
 exports.insertAgent = async (req, res, next) => {
@@ -16,7 +16,7 @@ exports.insertAgent = async (req, res, next) => {
     if (req.files.image && count === 0) {
       await cloudinaryUtil.v2.uploader.upload(
         req.files.image.path, {
-          folder: `${cloudinaryPath}/agents`
+          folder: cloudinaryPath
         },
         (err, imageInfo) => {
           if (err) {
@@ -57,11 +57,29 @@ exports.getAllAgents = async (req, res, next) => {
 
 // PATCH routes
 exports.patchAgentByID = async (req, res, next) => {
+  const body = {
+    ...req.body
+  }
   try {
-    const agent = await Agent.findByIdAndUpdate(req.params.agentID, req.body, {
+    if (req.files && req.files.image && req.files.image.path) {
+      const agent = await Agent.findById(req.params.agentID);
+      if (agent.imageID) {
+        await cloudinaryUtil.v2.uploader.destroy(agent.imageID, (error, result) => {
+          if (error) res.send(error);
+        })
+      }
+      const uploadResult = await cloudinaryUtil.v2.uploader.upload(
+        req.files.image.path, {
+          folder: cloudinaryPath
+        }
+      );
+      body.imageID = uploadResult.public_id
+      body.mediaURL = uploadResult.url
+    }
+    const result = await Agent.findByIdAndUpdate(req.params.agentID, body, {
       new: true
     });
-    res.send(agent);
+    res.send(result);
   } catch (error) {
     res.send(error);
   }
@@ -71,9 +89,11 @@ exports.patchAgentByID = async (req, res, next) => {
 exports.deleteAgentByID = async (req, res, next) => {
   try {
     const agent = await Agent.findByIdAndDelete(req.params.agentID);
-    await cloudinaryUtil.v2.uploader.destroy(agent.imageID, (error, result) => {
-      if (error) console.log('Failed to delete agent: ', agent.imageID);
-    });
+    if (agent.imageID) {
+      await cloudinaryUtil.v2.uploader.destroy(agent.imageID, (error, result) => {
+        if (error) console.log('Failed to delete agent: ', agent.imageID);
+      });
+    }
     res.send(agent);
   } catch (err) {
     res.send(err);
