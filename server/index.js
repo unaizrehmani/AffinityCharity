1608832a71daf992e13e4bf8ccbaa8a81f5d9eb9
Unaiz Rehmani <unaizrehmani@gmail.com>
2019-04-06 14:40:02 -0400

    refactor db connection and app

--- server/index.js
+++ server/index.js
@@ -1,44 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const sanitizeMongo = require('express-mongo-sanitize');
-const cors = require('cors');
-
-const app = express();
-
-app.use(sanitizeMongo());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-app.use(cors());
-
-// CONFIG
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME
-const MONGO_DB_USER = process.env.MONGO_DB_USER
-const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD
-const PORT = process.env.PORT
-
-// ROUTES
-app.use('/api', require('./routes/api'));
-
-// DB
-mongoose
-  .connect(
-    `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster0-jgsmf.mongodb.net/${MONGO_DB_NAME}?retryWrites=true`, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    },
-    () => {}
-  )
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () => {
-      console.log(`Server listening on port: ${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.log(err);
-    throw err;
-  });
+const connect = async () => {
+  await require("./util/db")();
+  require("./util/server");
+}
+connect();
+module.exports = connect;
