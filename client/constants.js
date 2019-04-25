e2b6a2a568fb746d98d8891587032e88a715f4dc
Pasoon Azimi <pasoon.azimi10@gmail.com>
2019-04-25 13:27:40 -0400

    added colors to constants

--- client/constants.js
+++ client/constants.js
@@ -1,4 +1,8 @@
 // COLOR SCHEMES
-
+export const BACKGROUND_COLOR = '#FFFFF';
+export const PRIMARY_COLOR = '#474B53';
+export const SECONDARY_COLOR = '#F2F2F2';
+export const PRIMARY_ACCENT_COLOR = '#E35268';
+export const SECONDARY_ACCENT_COLOR = '#56DDBE';
 
 // TEXT SCHEMES
