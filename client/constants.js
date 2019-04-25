ee65aaff558909e40bf7052f5be3aea2838e5b2a
Pasoon Azimi <pasoon.azimi10@gmail.com>
2019-04-25 13:31:03 -0400

    added text schemes to constants

--- client/constants.js
+++ client/constants.js
@@ -6,3 +6,22 @@ export const PRIMARY_ACCENT_COLOR = '#E35268';
 export const SECONDARY_ACCENT_COLOR = '#56DDBE';
 
 // TEXT SCHEMES
+export const HEADING = {
+    fontFamily: "Arial",
+    fontSize: "30px",
+}
+
+export const SUB_HEADING = {
+    fontFamily: "Arial",
+    fontSize: "20px",
+}
+
+export const BODY = {
+    fontFamily: "Arial",
+    fontSize: "14px",
+}
+
+export const SUB_BODY = {
+    fontFamily: "Arial",
+    fontSize: "10px",
+}
