a5a0d803864b651e85c990d5e3c2f2a11442154c
Zarif Shahriar <zarif.shahriar@gmail.com>
2019-04-27 20:14:14 -0400

    Modified components to include proptypes, added sign in button and statusbar color

---
+++ client/src/components/seperator.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { PRIMARY_ACCENT_COLOR, PRIMARY_COLOR } from '../../constants.js'
+import PropTypes from 'prop-types'
+
+export default class Seperator extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <View style={styles(this.props).seperator} />
+  }
+}
+
+const styles = props =>
+  StyleSheet.create({
+    seperator: {
+      height: 4,
+      width: props.isLarge ? 100 : 24,
+      backgroundColor: props.isPrimary ? PRIMARY_COLOR : PRIMARY_ACCENT_COLOR,
+      borderRadius: 2,
+      marginBottom: props.seperation,
+      marginTop: props.seperation
+    }
+  })
+
+Seperator.defaultProps = {
+  isPrimary: true,
+  isLarge: false,
+  seperation: 70
+}
+
+Seperator.propTypes = {
+  isPrimary: PropTypes.bool,
+  isLarge: PropTypes.bool,
+  seperation: PropTypes.number
+}
