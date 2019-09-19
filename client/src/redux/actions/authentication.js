270c7df0c040dfc14ea8aa9765c45001801940a1
Zarif Shahriar <zarif.shahriar@gmail.com>
2019-09-19 19:01:59 -0400

    Added Redux into app

---
+++ client/src/redux/actions/authentication.js
@@ -0,0 +1,13 @@
+// Types
+export const LOGIN_USER = 'LOGIN_USER';
+
+// Action Creators
+export function loginUser(username, password) {
+    return {
+      type: LOGIN_USER,
+      payload: {
+        username: username,
+        password: password
+      }
+    }
+  }
