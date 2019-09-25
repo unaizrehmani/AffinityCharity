b1a5e01c7dddabbd2c240a85cb8e6edbceb6a226
Zarif Shahriar <zarif.shahriar@gmail.com>
2019-09-25 13:49:11 -0400

    Renamed some files and changed project struct

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
+import LoginPage from './containers/loginPage'
+import NotFound from './containers/notFoundPage'
 
-function App() {
-	return (
-		<div className='App'>
-			<h1>Home</h1>
-		</div>
-	);
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/' component={LoginPage} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    )
+  }
 }
 const mapStateToProps = state => {
-	return {
-		isLoggedIn: state.authentication.isLoggedIn
-	};
-};
+  return {
+    isLoggedIn: state.authentication.isLoggedIn
+  }
+}
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
