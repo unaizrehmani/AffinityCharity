a0c53c081c484ffb66519c0a088d1d2d1a085780
Unaiz Rehmani <unaizrehmani@gmail.com>
2019-09-25 17:07:40 -0400

    #92 implement and add linter scripts

--- client/src/containers/notFoundPage.js
+++ client/src/containers/notFoundPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NotFoundPage extends Component {
   render() {
@@ -6,6 +6,6 @@ export default class NotFoundPage extends Component {
       <div>
         <h1>Error 404: Page not found</h1>;
       </div>
-    )
+    );
   }
 }
