97849fd0aa7fea11253c053138a3c41d49c3ef2f
Unaiz Rehmani <unaizrehmani@gmail.com>
2019-09-25 02:05:02 -0400

    #92 add react-email-editor

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import EmailEditor from 'react-email-editor';
 
-function App() {
-	return (
-		<div className='App'>
-			<h1>Home</h1>
-		</div>
-	);
+class App extends Component {
+	render = () => {
+		return (
+		<div>
+			<h1>hello world</h1>
+			<div>
+				<button onClick={this.exportHtml}>Export HTML</button>
+			</div>
+
+			<EmailEditor
+				ref={editor => this.editor = editor}
+			/>
+		</div>);
+	}
+
+	exportHtml = () => {
+		this.editor.exportHtml(data => {
+		  const { design, html } = data
+		  console.log('exportHtml', html)
+		})
+	  }
 }
 const mapStateToProps = state => {
 	return {
