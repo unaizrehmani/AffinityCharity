10706fee49958231b2c3c5c8f15af29a8a06fb12
Unaiz Rehmani <unaizrehmani@gmail.com>
2019-09-26 13:00:50 -0400

    #92 refactor backend email service

---
+++ server/api/controllers/email.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+exports.sendEmail = (req, res, next) => {
+  const user = process.env.CLIENT_EMAIL;
+  const pass = process.env.CLIENT_PASSWORD;
+  const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+      user,
+      pass
+    }
+  });
+  const mailOptions = {
+    to: 'unaizrehmani@gmail.com',
+    subject: 'Test',
+    text: 'Default text',
+    html: `${req.body}`
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(400).send('Email could not be sent');
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+    res.status(200).send('Email sent');
+  });
+};
--- server/api/routes/email.js
+++ server/api/routes/email.js
@@ -1,31 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const nodemailer = require('nodemailer');
+const emailController = require('../controllers/email');
+const multiPartMiddleware = require('connect-multiparty')();
 
-router.post('/send-email', (req, res, next) => {
-  const user = process.env.CLIENT_EMAIL;
-  const pass = process.env.CLIENT_PASSWORD;
-  const transporter = nodemailer.createTransport({
-    service: 'hotmail',
-    auth: {
-      user,
-      pass
-    }
-  });
-  const mailOptions = {
-    to: 'unaizrehmani@gmail.com',
-    subject: 'Test',
-    text: 'Default text',
-    html: `${req.body}`
-  };
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(400).send('Email could not be sent');
-    }
-    console.log('Message %s sent: %s', info.messageId, info.response);
-    res.status(200).send('Email sent');
-  });
-});
+router.post('/send-email', multiPartMiddleware, emailController.sendEmail);
 
 module.exports = router;
