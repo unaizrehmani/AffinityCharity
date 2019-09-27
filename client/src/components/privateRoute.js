1dd72321409334a9a1adf7c1b4e5c81188016940
Rami Taleb <rtale089@uottawa.ca>
2019-09-27 11:22:28 -0400

    add  private routes

---
+++ client/src/components/privateRoute.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+	Route,
+	Redirect
+} from "react-router-dom";
+
+// This needs to be changed to use redux store
+const authHere = false;
+
+export const PrivateRoute = ({ component: Component, ...rest }) => (
+	<Route {...rest} render={(props) => (
+		authHere === true
+			? <Component {...props} />
+			: <Redirect to='login' />
+	)} />
+);
