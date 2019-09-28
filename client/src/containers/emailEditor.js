920af65ca8dfa0a669160231e483740067fcfcb9
Unaiz Rehmani <unaizrehmani@gmail.com>
2019-09-28 17:27:19 -0400

    #134 add subject field to email editor

--- client/src/containers/emailEditor.js
+++ client/src/containers/emailEditor.js
@@ -3,18 +3,26 @@ import EmailEditor from "react-email-editor";
 import styled from 'styled-components';
 import axios from "axios";
 import MultipleEmail from '../components/multipleEmail';
-
+import { Input } from "semantic-ui-react";
+import Button from "../components/button";
 class Emailer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      emails: ['unaizrehmani@gmail.com']
+      emails: ['unaizrehmani@gmail.com'],
+      subject: ''
     }
   }
 
   updateEmails = emails => {
     this.setState({ emails });
   }
+
+  handleUserInput = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value });
+  };
 
   render = () => {
     return (
@@ -25,7 +33,18 @@ class Emailer extends Component {
           
         <MultipleEmailStyle>
           <MultipleEmail emails={this.state.emails} updateEmails={this.updateEmails} />
-          <button onClick={this.exportHtml}>Send Email</button>
+          <FormInputStyle
+              type="text"
+              name="subject"
+              value={this.state.subject}
+              onChange={this.handleUserInput}
+              label="subject"
+              placeholder="Enter email subject"
+            />
+          <br/><br/>
+          <ButtonStyle>
+            <Button title="Send Email" primary handleClick={this.exportHtml} />
+          </ButtonStyle>
         </MultipleEmailStyle>
       </div>
     );
@@ -35,7 +54,7 @@ class Emailer extends Component {
     this.editor.exportHtml(data => {
       const html = `${String(data.html)}`;
       const email = this.state.emails;
-      const subject = "Shefali Jain";
+      const subject = this.state.subject;
       axios
         .post(
           "https://social-charity-server.herokuapp.com/api/email/send-email",
@@ -62,5 +81,20 @@ const MultipleEmailStyle = styled.div`
   padding-right: 20px;
   padding-left: 20px;
   padding-top: 5px;
+`
+const FormInputStyle = styled(Input)`
+  input {
+    width: 220px;
+  }
+  .label {
+    width: 100px;
+  }
+`;
+
+const ButtonStyle = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
 `
 export default Emailer;
