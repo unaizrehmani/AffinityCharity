8112e83e9ee55fd0fed3f606f7d2aa13c606ac1e
Rami Taleb <rtale089@uottawa.ca>
2019-10-23 15:22:12 -0400

    add dummy example tests for now

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+
+// Enzyme example
+describe('First React component test with Enzyme', () => {
+  it('renders without crashing', () => {
+     shallow(<App />);
+   });
+});
+
+
+// Jest example
+const sum = (x, y) => x + y;
+
+describe('Examining the syntax of Jest tests', () => {
+  it('sums numbers', () => {
+		expect(sum(1, 2)).toEqual(3);
+		expect(sum(2, 2)).toEqual(4);
+   });
+});
