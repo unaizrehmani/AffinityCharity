6a6157a90a374ff87752f52cb5ac1259ca708f15
ZarifS <zarif.shahriar@gmail.com>
2019-10-29 21:01:52 -0400

    Linting

--- client/src/containers/homePage.js
+++ client/src/containers/homePage.js
@@ -69,14 +69,14 @@ export class HomePageContainer extends React.Component {
         </Header>
         <SearchContainer>
           <Input
-            name='searchBar'
-            icon='search'
-            placeholder='Search for a cause...'
+            name="searchBar"
+            icon="search"
+            placeholder="Search for a cause..."
             type={'text'}
             onChange={this.onQueryChange}
           />
-          <Link to='/createcause'>
-            <Button title='Create Cause' primary />
+          <Link to="/createcause">
+            <Button title="Create Cause" primary />
           </Link>
         </SearchContainer>
         <PinnedCauses>
