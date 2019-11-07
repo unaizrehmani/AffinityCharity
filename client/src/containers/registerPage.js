26aea2ad79d53ba3f9dd49432d82db160709989b
ZarifS <zarif.shahriar@gmail.com>
2019-11-06 20:29:01 -0500

    Update to register component

--- client/src/containers/registerPage.js
+++ client/src/containers/registerPage.js
@@ -53,7 +53,23 @@ export default class RegisterPageComponent extends Component {
   };
 
   submitEmail = () => {
-    alert('Added!');
+    const URL = `https://social-charity-server.herokuapp.com/api/donors`;
+    axios
+      .post(
+        URL,
+        {
+          email: this.state.email,
+          causeId: this.state.causeId
+        },
+        { 'Content-Type': 'application/json' }
+      )
+      .then(response => {
+        console.log(response.data);
+        alert("You've been subscribed successfully!");
+      })
+      .catch(error => {
+        console.log('error ' + error);
+      });
   };
 
   render() {
@@ -85,19 +101,19 @@ export default class RegisterPageComponent extends Component {
           <InputContainer>
             <InputTitle>Your Email</InputTitle>
             <Input
-              id="input-email"
-              name="email"
-              size="large"
-              type="email"
-              placeholder="john.doe@email.com"
+              id='input-email'
+              name='email'
+              size='large'
+              type='email'
+              placeholder='john.doe@email.com'
               value={this.state.email}
               onChange={this.handleUserInput}
               noLabel={true}
             />
           </InputContainer>
           <Button
-            id="button-subscribe"
-            title="Subscribe"
+            id='button-subscribe'
+            title='Subscribe'
             primary
             handleClick={this.submitEmail}
           />
