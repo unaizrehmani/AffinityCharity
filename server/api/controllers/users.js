7358e5af2226055f2088e204997e2ceb064fb237
Unaiz Rehmani <unaizrehmani@gmail.com>
2019-11-07 00:33:46 -0500

    #182 update and pass error message if incorrect credentials

--- server/api/controllers/users.js
+++ server/api/controllers/users.js
@@ -96,10 +96,7 @@ exports.getAllUsers = async (req, res, next) => {
  * @param {file} image
  */
 exports.patchUserByID = async (req, res, next) => {
-  const body = {
-    ...req.body
-  };
-  console.log('body: ', body);
+  const body = { ...req.body };
   try {
     if (req.files && req.files.image && req.files.image.path) {
       const user = await User.findById(req.params.userID);
@@ -121,14 +118,18 @@ exports.patchUserByID = async (req, res, next) => {
       body.mediaURL = uploadResult.url;
     }
 
-    if (body.newPassword) {
-      const result = await User.authenticate(body.oldEmail, body.oldPassword);
+    const { oldEmail, oldPassword, newPassword } = body;
+    if (oldEmail && oldPassword && newPassword) {
+      const result = await User.authenticate(oldEmail, oldPassword);
       if (result != null) {
-        body.password = await bcrypt.hash(body.newPassword, SALTROUNDS);
-        body.email = body.newEmail;
+        body.password = await bcrypt.hash(newPassword, SALTROUNDS);
+      } else {
+        res.send({
+          errorHeader: 'Incorrect password',
+          errorContent:
+            'Please provide the current password before updating the password'
+        });
       }
-    } else {
-      delete body.password;
     }
 
     const result = await User.findByIdAndUpdate(req.params.userID, body, {
