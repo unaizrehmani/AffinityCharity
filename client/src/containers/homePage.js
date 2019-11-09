946d68e5131273779d304e232045b158c07a3062
Pasoon <pasoon.azimi10@gmail.com>
2019-11-08 19:58:08 -0500

    removed homepage tests until rework is done for new additions

--- client/src/containers/homePage.js
+++ client/src/containers/homePage.js
@@ -12,8 +12,7 @@ export class HomePageContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: '',
-      causes: []
+      query: ''
     };
   }
 
