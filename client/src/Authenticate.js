1a725f5d9eb507ff72caab27bdad0133d5852975
ZarifS <zarif.shahriar@gmail.com>
2019-11-10 00:37:46 -0500

    Added unsubscribe route

--- client/src/Authenticate.js
+++ client/src/Authenticate.js
@@ -4,15 +4,17 @@ import App from './App';
 import PrivateRoute from './components/privateRoute';
 import LoginPage from './containers/loginPage';
 import RegisterPage from './containers/registerPage';
+import UnsubscribePage from './containers/unsubscribePage';
 
 export default class Authenticate extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/login" component={LoginPage} />
-          <Route path="/register/:id" component={RegisterPage} />
-          <PrivateRoute path="/" component={<App />} />
+          <Route exact path='/login' component={LoginPage} />
+          <Route path='/register/:id' component={RegisterPage} />
+          <Route path='/unsubscribe/:id' component={UnsubscribePage} />
+          <PrivateRoute path='/' component={<App />} />
         </Switch>
       </Router>
     );
